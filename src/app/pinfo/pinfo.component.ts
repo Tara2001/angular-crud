a69379da76c1a981844565c171da83aa9db49273
Tara2001 <darshanamaru1432@gmail.com>
2023-07-07 18:45:21 +0530

    Validations

--- src/app/pinfo/pinfo.component.ts
+++ src/app/pinfo/pinfo.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Ser01Service } from '../services/ser01.service';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -15,11 +15,25 @@ export class PinfoComponent implements OnInit {
   id: any;
   tdata: any;
   cond: boolean = false;
+  submitted: boolean = false;
 
-  constructor(private entries: FormBuilder, private services: Ser01Service, private next: Router, private route: ActivatedRoute) { }
+  constructor(
+    private entries: FormBuilder,
+    private services: Ser01Service,
+    private next: Router,
+    private route: ActivatedRoute) { }
 
   ngOnInit(): void {
     debugger
+
+    this.information = this.entries.group({
+      fname: ['', [Validators.required]],
+      lname: ['', [Validators.required, Validators.pattern('^[a-zA-Z]*$')]],
+      location: ['', [Validators.required, Validators.pattern('^[a-zA-Z]*$')]],
+      contact: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      email: ['', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$')]]
+    })
+
     this.route.paramMap.subscribe((params) => {
       debugger
       this.id = params.get("id");
@@ -27,14 +41,8 @@ export class PinfoComponent implements OnInit {
         this.cond = true
       }
     })
-    this.information = this.entries.group({
-      fname: [''],
-      lname: [''],
-      location: [''],
-      contact: [''],
-      email: ['']
-    })
-    if (this.id != undefined || this.id != null || this.id != '') {
+
+    if (this.id != undefined && this.id != null && this.id != '') {
       this.services.getbyid(this.id).subscribe((result: any) => {
         debugger
         this.tdata = result;
@@ -44,23 +52,37 @@ export class PinfoComponent implements OnInit {
     }
   }
 
+  get s() {
+    return this.information.controls
+  }
+
   submit() {
     debugger
+    this.submitted = true
+    if (this.information.invalid) {
+      return;
+    }
+
     if (this.cond == false) {
       this.pinfodata = this.information.value;
 
-      this.services.postdata(this.pinfodata).subscribe((result: any) => {
-        debugger
-
-        this.next.navigateByUrl('sinfo')
-      })
+      if (confirm("Are you sure want to proceed?")) {
+        this.services.postdata(this.pinfodata).subscribe((result: any) => {
+          debugger
+          alert('Registered successfully !')
+          this.next.navigateByUrl('sinfo')
+        })
+      }
     }
     else {
       this.pinfodata = this.information.value;
-      this.services.update(this.pinfodata, this.id).subscribe((result: any) => {
-        debugger
-        this.next.navigateByUrl('sinfo')
-      })
+      if (confirm("Are you sure want to update?")) {
+        this.services.update(this.pinfodata, this.id).subscribe((result: any) => {
+          debugger
+          alert('Record updated successfully !')
+          this.next.navigateByUrl('sinfo')
+        })
+      }
     }
 
   }
